d5a0fca336bd224d32cddbc9b4da5396c62fff8c
iampradiptaghosh <iampradiptaghosh@gmail.com>
2017-03-07 13:04:05 -0800

    improved the script

--- load_prog.py
+++ load_prog.py
@@ -2,6 +2,7 @@ from pyOCD.board import MbedBoard
 import os, sys
 import logging
 import serial.tools.list_ports_linux as serial_tools
+import glob
 
 def serial_ports():
     ports = list(serial_tools.comports())
@@ -29,8 +30,10 @@ target.halt()
 # target.step()
 # print "pc: 0x%X" % target.readCoreRegister("pc")
 #   pc: 0xA32
+filename=glob.glob('./BUILD/LPC1768/GCC_ARM/*.bin')
 
-flash.flashBinary("./BUILD/LPC1768/GCC_ARM/polulu_test.bin")
+
+flash.flashBinary(filename[0])
 print("pc: 0x%X" % target.readCoreRegister("pc"))
 #   pc: 0x10000000
 
